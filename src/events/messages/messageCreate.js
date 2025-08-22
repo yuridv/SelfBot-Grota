1bba7a9c3ee39d9fdf0412a4dc83268af800d766
Yuri Eduardo <57859317+yuridv@users.noreply.github.com>
2025-08-22 17:53:30 -0300

    send log automatic

--- src/events/messages/messageCreate.js
+++ src/events/messages/messageCreate.js
@@ -1,16 +1,20 @@
 const { Errors } = require('../../utils/functions');
 const config = require('../../../config.json');
 
-const { MessageEmbed } = require('discord.js-selfbot-v13');
+const { WebhookClient, EmbedBuilder } = require('discord.js');
 
 const event = (client, message) => {
   try {
-    if (message.author.id === '559681569325973505' && message.content === '!status') {
+    if (message.author.id === config.developer && message.content === '!status') {
       return message.channel.send('> **Status:**\n・ *__ONLINE__*');
     }
 
-    if (message.author.id === '1380336491859017819' && message.embeds && message.embeds[0]) {
-      client.channels.cache.get('1363753126171115571').send({ embeds: message.embeds });
+    if (message.author.id === config.bot_city && message.embeds && message.embeds[0]) {
+      const webhook = new WebhookClient({ url: config.webhook_log_chest });
+
+      webhook.send({
+        embeds: message.embeds
+      });
     }
   } catch (err) {
     return Errors(err, `Event ${__filename}`)
