4f8babf5411e4c41d41b69e8028924e8284e564c
Yuri Eduardo <57859317+yuridv@users.noreply.github.com>
2025-08-25 10:15:10 -0300

    fix errors

--- src/events/messages/messageCreate.js
+++ src/events/messages/messageCreate.js
@@ -3,16 +3,16 @@ const config = require('../../../config.json');
 
 const { WebhookClient, EmbedBuilder } = require('discord.js');
 
-const event = (client, message) => {
+const event = async(client, message) => {
   try {
     if (message.author.id === config.developer && message.content === '!status') {
       return message.channel.send('> **Status:**\n・ *__ONLINE__*');
     }
-    
+
     if ((message.channel.id === config.channel_logs_leader || message.channel.id === config.channel_logs_member) && message.embeds && message.embeds[0]) {
       const webhook = new WebhookClient({ url: config.webhook_log_chest });
 
-      webhook.send({
+      await webhook.send({
         embeds: message.embeds
       });
     }
